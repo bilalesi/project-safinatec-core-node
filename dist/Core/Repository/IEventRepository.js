a98f2603d6c93f4fb0c8e37495fa3c9162b1521e
bilalesi <b_meddah@esi.dz>
2021-06-29 09:15:38 +0100

    remove dist file from repo

--- dist/Core/Repository/IEventRepository.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=IEventRepository.js.map
--- src/main.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Http_1 = require("./Infrastructure/Http");
-Http_1["default"]();
